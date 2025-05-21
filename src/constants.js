27d7e98da6f590cdf48c7711992f420a6c0fa3dd
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-05-21 16:59:54 +0530

    change some content

--- src/constants.js
+++ src/constants.js
@@ -184,7 +184,7 @@ export const SkillsInfo = [
       school: "DHARMSINH DESAI UNIVERSITY , Nadiad",
       date: "Oct 2022– May 2026",
       grade: "73.2%",
-      desc: "I completed my Bachelor's degree in Computer Science (B.TECH) from DDIT, Nadiad. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development.",
+      desc: "I'm currently pursuing my Bachelor's degree in Information and Technology (B.TECH) from DDIT, Nadiad. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development.",
       degree: " Bachelor of Technology  - B.TECH (Information Technology)",
     },
     {
